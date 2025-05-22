20817de8f969023f0caf8747a48c2b6a62b229b4
Farias1douglas <douglas.v@escolar.ifrn.edu.br>
2025-05-22 20:55:22 +0000

    Qualquer coisa

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
+idade = 2
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -20,6 +20,20 @@ def terceiro():
 @app.route('/quarto')
 def quarto():
     return render_template('quarto.html')
+#Abaixo teste de formulario#
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+@app.route('/recebedados', methods=['GET'])
+def recebedados():
+    nome = request.args.get("nome")
+    email = request.args.get("email")
+    return "{} - {}".format(nome, email)
+#Abaixo ser teste de Condicionais#
+@app.route('/Verificador/<int:idade>')
+def verificador(idade):
+    return "você tem" + idade +"de idade."
 
 if __name__ == '__main__':
     app.run(debug=True)
